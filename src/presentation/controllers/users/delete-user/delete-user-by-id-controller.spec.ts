911281842e875741e714b1d83e5a923ad7d3e05a
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2024-06-10 12:36:43 -0300

    refactor: move sut to a creation factory method

--- src/presentation/controllers/users/delete-user/delete-user-by-id-controller.spec.ts
+++ src/presentation/controllers/users/delete-user/delete-user-by-id-controller.spec.ts
@@ -2,25 +2,47 @@ import { DeleteUserById } from '../../../../domain/usecases/users/delete-user-by
 import { Either, right } from '../../../../shared'
 import { describe, it, vi, expect } from 'vitest'
 import { DeleteUserByIdController } from './delete-user-by-id-controller'
+import { HttpRequest } from '../../../protocols/http'
+
+const makeHttpRequest = (): HttpRequest => ({
+  body: { userId: 'any_id' }
+})
+
+const makeDeleteUserByIdUseCaseStub = (): DeleteUserById => {
+  class DeleteUserByIdUseCaseStub implements DeleteUserById {
+    async execute(
+      _userId: string
+    ): Promise<Either<Error, DeleteUserById.Result>> {
+      return right({
+        id: 'any_id',
+        name: 'any_name',
+        email: 'any_email',
+        phone: 'any_phone'
+      })
+    }
+  }
+  return new DeleteUserByIdUseCaseStub()
+}
+
+type SutTypes = {
+  sut: DeleteUserByIdController
+  deleteUserByIdUseCaseStub: DeleteUserById
+}
+
+const makeSut = (): SutTypes => {
+  const deleteUserByIdUseCaseStub = makeDeleteUserByIdUseCaseStub()
+  const sut = new DeleteUserByIdController(deleteUserByIdUseCaseStub)
+  return {
+    sut,
+    deleteUserByIdUseCaseStub
+  }
+}
 
 describe('DeleteUserByIdController', () => {
   it('Should call DeleteUserByIdUseCase with correct param', async () => {
-    class DeleteUserByIdUseCaseStub implements DeleteUserById {
-      async execute(
-        _userId: string
-      ): Promise<Either<Error, DeleteUserById.Result>> {
-        return right({
-          id: 'any_id',
-          name: 'any_name',
-          email: 'any_email',
-          phone: 'any_phone'
-        })
-      }
-    }
-    const deleteUserByIdUseCaseStub = new DeleteUserByIdUseCaseStub()
-    const sut = new DeleteUserByIdController(deleteUserByIdUseCaseStub)
+    const { sut, deleteUserByIdUseCaseStub } = makeSut()
     const deleteUserSpy = vi.spyOn(deleteUserByIdUseCaseStub, 'execute')
-    await sut.handle({ body: { userId: 'any_id' } })
+    await sut.handle(makeHttpRequest())
     expect(deleteUserSpy).toHaveBeenCalledOnce()
     expect(deleteUserSpy).toHaveBeenCalledWith('any_id')
   })
