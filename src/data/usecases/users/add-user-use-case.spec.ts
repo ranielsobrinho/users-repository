c8ff0cf4518001415467063915b679772633bf66
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2024-06-10 12:35:17 -0300

    refactor: move sut creation to a factory method

--- src/data/usecases/users/add-user-use-case.spec.ts
+++ src/data/usecases/users/add-user-use-case.spec.ts
@@ -3,27 +3,48 @@ import { Either, right } from '../../../shared'
 import { GetUserByEmailRepository } from '../../protocols/users/get-user-by-email-repository'
 import { CreateUserUseCase } from './add-user-use-case'
 
+const makeCreateUserRequest = () => ({
+  name: 'any_name',
+  email: 'any_email',
+  phone: 'any_phone'
+})
+
+const makeGetUserByEmailRepositoryStub = (): GetUserByEmailRepository => {
+  class GetUserByEmailRepositoryStub implements GetUserByEmailRepository {
+    async getByEmail(
+      _email: string
+    ): Promise<Either<Error, GetUserByEmailRepository.Result>> {
+      return right(null)
+    }
+  }
+  return new GetUserByEmailRepositoryStub()
+}
+
+type SutTypes = {
+  sut: CreateUserUseCase
+  getUserByEmailRepositoryStub: GetUserByEmailRepository
+}
+
+const makeSut = (): SutTypes => {
+  const getUserByEmailRepositoryStub = makeGetUserByEmailRepositoryStub()
+  const sut = new CreateUserUseCase(getUserByEmailRepositoryStub)
+  return {
+    sut,
+    getUserByEmailRepositoryStub
+  }
+}
+
 describe('CreateUserUseCase', () => {
   it('Should call GetUserByEmailRepository with correct param', async () => {
-    class GetUserByEmailRepositoryStub implements GetUserByEmailRepository {
-      async getByEmail(
-        _email: string
-      ): Promise<Either<Error, GetUserByEmailRepository.Result>> {
-        return right(null)
-      }
-    }
-    const getUserByEmailRepositoryStub = new GetUserByEmailRepositoryStub()
-    const sut = new CreateUserUseCase(getUserByEmailRepositoryStub)
+    const { sut, getUserByEmailRepositoryStub } = makeSut()
     const getUserByEmailSpy = vi.spyOn(
       getUserByEmailRepositoryStub,
       'getByEmail'
     )
-    await sut.execute({
-      name: 'any_name',
-      email: 'any_email',
-      phone: 'any_phone'
-    })
+    await sut.execute(makeCreateUserRequest())
     expect(getUserByEmailSpy).toHaveBeenCalled()
-    expect(getUserByEmailSpy).toHaveBeenCalledWith('any_email')
+    expect(getUserByEmailSpy).toHaveBeenCalledWith(
+      makeCreateUserRequest().email
+    )
   })
 })
