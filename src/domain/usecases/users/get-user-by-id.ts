3ee4b36f3e116e551b0d5ea8207e1bd74b68ab82
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2024-06-10 12:36:11 -0300

    feat: add domain use case to get user by id

---
+++ src/domain/usecases/users/get-user-by-id.ts
@@ -0,0 +1,10 @@
+import { UserModel } from '@/domain/models/user-model'
+import { Either } from '@/shared'
+
+export interface GetUserById {
+  execute(userId: string): Promise<Either<Error, GetUserById.Result>>
+}
+
+export namespace GetUserById {
+  export type Result = UserModel
+}
