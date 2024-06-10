0615af36ae16f42ea08b4e73eff8a64c6fb6d033
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2024-06-10 12:36:04 -0300

    refactor: move sut creation to a factory method

--- src/presentation/controllers/users/list-all-users/list-all-users-controller.spec.ts
+++ src/presentation/controllers/users/list-all-users/list-all-users-controller.spec.ts
@@ -3,22 +3,43 @@ import { ListAllUsers } from '../../../../domain/usecases/users/list-all-users'
 import { Either, right } from '../../../../shared'
 import { ListAllUsersController } from './list-all-users-controller'
 
+const makeListAllUsersResult = () => {
+  return [
+    {
+      id: 'any_id',
+      name: 'any_name',
+      email: 'any_email',
+      phone: 'any_phone'
+    }
+  ]
+}
+
+const makeListAllUsersUseCaseStub = (): ListAllUsers => {
+  class ListAllUsersUseCaseStub implements ListAllUsers {
+    async execute(): Promise<Either<Error, ListAllUsers.Result>> {
+      return right(makeListAllUsersResult())
+    }
+  }
+  return new ListAllUsersUseCaseStub()
+}
+
+type SutTypes = {
+  sut: ListAllUsersController
+  listAllUsersUseCaseStub: ListAllUsers
+}
+
+const makeSut = (): SutTypes => {
+  const listAllUsersUseCaseStub = makeListAllUsersUseCaseStub()
+  const sut = new ListAllUsersController(listAllUsersUseCaseStub)
+  return {
+    sut,
+    listAllUsersUseCaseStub
+  }
+}
+
 describe('ListAllUsersController', () => {
   it('Should call ListAllUsersUseCase once', async () => {
-    class ListAllUsersUseCaseStub implements ListAllUsers {
-      async execute(): Promise<Either<Error, ListAllUsers.Result>> {
-        return right([
-          {
-            id: 'any_id',
-            name: 'any_name',
-            email: 'any_email',
-            phone: 'any_phone'
-          }
-        ])
-      }
-    }
-    const listAllUsersUseCaseStub = new ListAllUsersUseCaseStub()
-    const sut = new ListAllUsersController(listAllUsersUseCaseStub)
+    const { sut, listAllUsersUseCaseStub } = makeSut()
     const listUsersSpy = vi.spyOn(listAllUsersUseCaseStub, 'execute')
     await sut.handle({})
     expect(listUsersSpy).toHaveBeenCalledOnce()
