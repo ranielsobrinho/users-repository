ab66300828e7d1529c4d1943862cfebb3a77bb71
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2024-06-10 12:35:35 -0300

    chore: add dotenv package and changed client to private constant

--- src/infra/database/postgres/helpers/postgres-helper.ts
+++ src/infra/database/postgres/helpers/postgres-helper.ts
@@ -1,27 +1,24 @@
 import { Client } from 'pg'
 
-const DB_CONNECTION_STRING: string =
-  process.env.DB_CONNECTION_STRING ||
-  'postgresql://postgres:95751535r@localhost:5432/postgres'
+const DB_CONNECTION_STRING: string = process.env.DB_CONNECTION_STRING!
 
 class PostgresHelper {
+  private client = new Client({ connectionString: DB_CONNECTION_STRING })
+
   async connect(): Promise<void> {
-    const client = new Client({ connectionString: DB_CONNECTION_STRING })
-    client
+    this.client
       .connect()
       .then(() => console.log('Postgres connected !'))
       .catch((err) => console.error('Error when connecting to postgres!', err))
   }
 
   async getClient(): Promise<Client> {
-    const client = new Client({ connectionString: DB_CONNECTION_STRING })
-    await client.connect()
-    return client
+    await this.client.connect()
+    return this.client
   }
 
   async disconnect(): Promise<void> {
-    const client = new Client({ connectionString: DB_CONNECTION_STRING })
-    client.end()
+    this.client.end()
   }
 }
 
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,1 +1,5 @@
 require('module-alias/register')
+require('dotenv').config()
+import DatabaseHelper from '../infra/database/postgres/helpers/postgres-helper'
+
+DatabaseHelper.connect()
