c7e449d88e8a7fd081f9c42737926934d5c32870
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2024-06-10 12:35:43 -0300

    feat: ensure CreateUserController returns 400 if CreateUser returns email already in use error

--- src/presentation/controllers/users/create-user-controller.ts
+++ src/presentation/controllers/users/create-user-controller.ts
@@ -1,18 +1,26 @@
 import { Controller } from '@/presentation/protocols/controller'
 import { HttpRequest, HttpResponse } from '@/presentation/protocols'
 import { CreateUser } from '@/domain/usecases/users/create-user'
-import { noContent, serverError } from '@/presentation/helpers/http-helper'
+import {
+  badRequest,
+  noContent,
+  serverError
+} from '@/presentation/helpers/http-helper'
 
 export class CreateUserController implements Controller {
   constructor(private readonly createUserUseCase: CreateUser) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const { name, email, phone } = httpRequest.body
-      await this.createUserUseCase.execute({
+      const response = await this.createUserUseCase.execute({
         name,
         email,
         phone
       })
+
+      if (response.isLeft()) {
+        return badRequest(response.value)
+      }
       return noContent()
     } catch (error) {
       return serverError(error as unknown as Error)
