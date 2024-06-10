fb11cfac5c933dbb7df645d7658129a8174b5a74
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2024-06-10 12:36:30 -0300

    chore: improve coverage of code

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,23 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   test: {
-    globals: true
+    globals: true,
+    coverage: {
+      include: [
+        '**/src/presentation/**',
+        '**/src/data/**',
+        '**/src/infra/**',
+        '**/src/main/**'
+      ],
+      exclude: [
+        '**/src/main/factories/**',
+        '**/src/main/server.ts',
+        '**/src/presentation/protocols/index.ts',
+        '**/src/main/config/module-alias.ts',
+        '**/src/main/adapters/**',
+        '**/src/infra/database/postgres/helpers/**'
+      ]
+    }
   },
   plugins: [tsconfigPaths()]
 })
