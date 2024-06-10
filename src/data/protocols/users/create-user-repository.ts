6c0061c8a2eeba0d0900bbaf19c8f935cad51e75
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2024-06-10 12:35:26 -0300

    refactor: change CreateUserRepository return object

--- src/data/protocols/users/create-user-repository.ts
+++ src/data/protocols/users/create-user-repository.ts
@@ -8,5 +8,5 @@ export interface CreateUserRepository {
 
 export namespace CreateUserRepository {
   export type Params = Omit<UserModel, 'id'>
-  export type Result = UserModel
+  export type Result = UserModel | null
 }
