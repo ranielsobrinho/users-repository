de871b9f51ee90940857bd247fa52862db74d26d
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2024-06-10 12:35:57 -0300

    refactor: move sut creation to a factory method

--- src/data/usecases/users/list-all-users/list-all-users-use-case.spec.ts
+++ src/data/usecases/users/list-all-users/list-all-users-use-case.spec.ts
@@ -2,23 +2,40 @@ import { describe, it, vi, expect } from 'vitest'
 import { ListAllUsersRepository } from '../../../protocols/users/list-all-users-repository'
 import { ListAllUsersUseCase } from './list-all-users-use-case'
 
+const makeListAllUsersRepositoryStub = (): ListAllUsersRepository => {
+  class ListAllUsersRepositoryStub implements ListAllUsersRepository {
+    async listAll(): Promise<ListAllUsersRepository.Result> {
+      return [
+        {
+          id: 'any_id',
+          name: 'any_name',
+          email: 'any_email@mail.com',
+          phone: '9789743413'
+        }
+      ]
+    }
+  }
+  return new ListAllUsersRepositoryStub()
+}
+
+type SutTypes = {
+  sut: ListAllUsersUseCase
+  listAllUsersRepositoryStub: ListAllUsersRepository
+}
+
+const makeSut = (): SutTypes => {
+  const listAllUsersRepositoryStub = makeListAllUsersRepositoryStub()
+  const sut = new ListAllUsersUseCase(listAllUsersRepositoryStub)
+  return {
+    sut,
+    listAllUsersRepositoryStub
+  }
+}
+
 describe('ListAllUsersUseCase', () => {
   it('Should call ListAllUsersRepository once', async () => {
-    class ListAllUsersRepositoryStub implements ListAllUsersRepository {
-      async listAll(): Promise<ListAllUsersRepository.Result> {
-        return [
-          {
-            id: 'any_id',
-            name: 'any_name',
-            email: 'any_email@mail.com',
-            phone: '9789743413'
-          }
-        ]
-      }
-    }
-    const listAllUsersRepository = new ListAllUsersRepositoryStub()
-    const sut = new ListAllUsersUseCase(listAllUsersRepository)
-    const listAllSpy = vi.spyOn(listAllUsersRepository, 'listAll')
+    const { sut, listAllUsersRepositoryStub } = makeSut()
+    const listAllSpy = vi.spyOn(listAllUsersRepositoryStub, 'listAll')
     await sut.execute()
     expect(listAllSpy).toHaveBeenCalledOnce()
   })
