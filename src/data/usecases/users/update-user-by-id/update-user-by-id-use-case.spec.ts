0b44bd854551d909d0bcf2b2d3fa59fc0f5681e5
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2024-06-10 12:36:50 -0300

    refactor: move sut to a creation factory method

--- src/data/usecases/users/update-user-by-id/update-user-by-id-use-case.spec.ts
+++ src/data/usecases/users/update-user-by-id/update-user-by-id-use-case.spec.ts
@@ -1,21 +1,41 @@
 import { describe, it, vi, expect } from 'vitest'
 import { GetUserByIdRepository } from '../../../protocols/users/get-user-by-id-repository'
 import { UpdateUserByIdUseCase } from './update-user-by-id-use-case'
+import { UserModel } from '../../../../domain/models/user-model'
+
+const makeUserModel = (): UserModel => ({
+  id: 'any_id',
+  name: 'any_name',
+  email: 'any_email',
+  phone: 'any_phone'
+})
+
+const makeGetUserByIdRepositoryStub = (): GetUserByIdRepository => {
+  class GetUserByIdRepositoryStub implements GetUserByIdRepository {
+    async getById(_userId: string): Promise<GetUserByIdRepository.Result> {
+      return makeUserModel()
+    }
+  }
+  return new GetUserByIdRepositoryStub()
+}
+
+type SutTypes = {
+  sut: UpdateUserByIdUseCase
+  getUserByIdRepositoryStub: GetUserByIdRepository
+}
+
+const makeSut = (): SutTypes => {
+  const getUserByIdRepositoryStub = makeGetUserByIdRepositoryStub()
+  const sut = new UpdateUserByIdUseCase(getUserByIdRepositoryStub)
+  return {
+    sut,
+    getUserByIdRepositoryStub
+  }
+}
 
 describe('UpdateUserByIdUseCase', () => {
   it('Should call GetUserByIdRepository with correct param', async () => {
-    class GetUserByIdRepositoryStub implements GetUserByIdRepository {
-      async getById(_userId: string): Promise<GetUserByIdRepository.Result> {
-        return {
-          id: 'any_id',
-          name: 'any_name',
-          email: 'any_email',
-          phone: 'any_phone'
-        }
-      }
-    }
-    const getUserByIdRepositoryStub = new GetUserByIdRepositoryStub()
-    const sut = new UpdateUserByIdUseCase(getUserByIdRepositoryStub)
+    const { sut, getUserByIdRepositoryStub } = makeSut()
     const getUserByIdSpy = vi.spyOn(getUserByIdRepositoryStub, 'getById')
     await sut.execute('any_id', {
       name: 'other_name',
