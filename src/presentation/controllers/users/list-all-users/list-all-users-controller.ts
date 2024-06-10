bf15ddf7e46c6a5aea95c927877c36f3055a008e
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2024-06-10 12:36:07 -0300

    feat: ensure ListAllUsersController returns 200 if ListAllUsers use case returns filled array

--- src/presentation/controllers/users/list-all-users/list-all-users-controller.ts
+++ src/presentation/controllers/users/list-all-users/list-all-users-controller.ts
@@ -1,12 +1,15 @@
 import { ListAllUsers } from '@/domain/usecases/users/list-all-users'
-import { noContent, serverError } from '@/presentation/helpers/http-helper'
+import { noContent, ok, serverError } from '@/presentation/helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
 
 export class ListAllUsersController implements Controller {
   constructor(private readonly listAllUsersUseCase: ListAllUsers) {}
-  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+  async handle(_httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      await this.listAllUsersUseCase.execute()
+      const response = await this.listAllUsersUseCase.execute()
+      if (response.isRight() && response.value.length >= 1) {
+        return ok(response.value)
+      }
       return noContent()
     } catch (error) {
       return serverError(error)
