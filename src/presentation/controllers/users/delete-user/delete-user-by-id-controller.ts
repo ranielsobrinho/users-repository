bc7890c3a8960a29eedf4147802bac0b6add07c6
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2024-06-10 12:36:44 -0300

    feat: ensure DeleteUserByIdController returns 500 if DeleteUserByIdUseCase throws

--- src/presentation/controllers/users/delete-user/delete-user-by-id-controller.ts
+++ src/presentation/controllers/users/delete-user/delete-user-by-id-controller.ts
@@ -1,12 +1,16 @@
 import { DeleteUserById } from '@/domain/usecases/users/delete-user-by-id'
-import { noContent } from '@/presentation/helpers/http-helper'
+import { noContent, serverError } from '@/presentation/helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
 
 export class DeleteUserByIdController implements Controller {
   constructor(private readonly deleteUserByIdUseCase: DeleteUserById) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { userId } = httpRequest.body
-    await this.deleteUserByIdUseCase.execute(userId)
-    return noContent()
+    try {
+      const { userId } = httpRequest.body
+      await this.deleteUserByIdUseCase.execute(userId)
+      return noContent()
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
