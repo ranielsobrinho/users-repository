cf3913cc612aca258d9761b67c3c93673ef5c4ee
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2024-06-10 12:35:39 -0300

    feat: create http helper to hold all return http contents

---
+++ src/presentation/helpers/http-helper.ts
@@ -0,0 +1,22 @@
+import { ServerError } from '../errors/server-error'
+import { HttpResponse } from '../protocols'
+
+export const badRequest = (error: Error): HttpResponse => ({
+  statusCode: 400,
+  body: error
+})
+
+export const serverError = (error: Error): HttpResponse => ({
+  statusCode: 500,
+  body: new ServerError(error.stack?.toString() ?? error.message)
+})
+
+export const ok = (data: any): HttpResponse => ({
+  statusCode: 200,
+  body: data
+})
+
+export const noContent = (): HttpResponse => ({
+  statusCode: 204,
+  body: null
+})
