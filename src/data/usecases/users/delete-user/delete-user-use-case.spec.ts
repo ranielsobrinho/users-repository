3c5e52cd93f9656660b0acf65d631f0c6f94409b
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2024-06-10 12:36:33 -0300

    refactor: move sut creation to a factory method

--- src/data/usecases/users/delete-user/delete-user-use-case.spec.ts
+++ src/data/usecases/users/delete-user/delete-user-use-case.spec.ts
@@ -1,21 +1,41 @@
 import { describe, vi, expect, it } from 'vitest'
 import { GetUserByIdRepository } from '../../../protocols/users/get-user-by-id-repository'
 import { DeleteUserUseCase } from './delete-user-use-case'
+import { UserModel } from '../../../../domain/models/user-model'
+
+const makeUserModel = (): UserModel => ({
+  id: 'any_id',
+  name: 'any_name',
+  email: 'any_email',
+  phone: 'any_phone'
+})
+
+const makeGetUserByIdRepositoryStub = (): GetUserByIdRepository => {
+  class GetUserByIdRepositoryStub implements GetUserByIdRepository {
+    async getById(_userId: string): Promise<GetUserByIdRepository.Result> {
+      return makeUserModel()
+    }
+  }
+  return new GetUserByIdRepositoryStub()
+}
+
+type SutTypes = {
+  sut: DeleteUserUseCase
+  getUserByIdRepositoryStub: GetUserByIdRepository
+}
+
+const makeSut = (): SutTypes => {
+  const getUserByIdRepositoryStub = makeGetUserByIdRepositoryStub()
+  const sut = new DeleteUserUseCase(getUserByIdRepositoryStub)
+  return {
+    sut,
+    getUserByIdRepositoryStub
+  }
+}
 
 describe('DeleteUserUseCase', () => {
   it('Should call GetUserByIdRepository with correct param', async () => {
-    class GetUserByIdRepositoryStub implements GetUserByIdRepository {
-      async getById(userId: string): Promise<GetUserByIdRepository.Result> {
-        return {
-          id: 'any_id',
-          name: 'any_name',
-          email: 'any_email',
-          phone: 'any_phone'
-        }
-      }
-    }
-    const getUserByIdRepositoryStub = new GetUserByIdRepositoryStub()
-    const sut = new DeleteUserUseCase(getUserByIdRepositoryStub)
+    const { sut, getUserByIdRepositoryStub } = makeSut()
     const getByIdSpy = vi.spyOn(getUserByIdRepositoryStub, 'getById')
     await sut.execute('any_id')
     expect(getByIdSpy).toHaveBeenCalledOnce()
