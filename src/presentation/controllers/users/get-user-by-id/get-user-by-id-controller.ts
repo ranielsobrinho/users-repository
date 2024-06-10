b3abf26c4d701cdee98986c554cbe68e90a9dfc2
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2024-06-10 12:36:23 -0300

    feat: ensure GetUserByIdController returns 500 if GetUserByIdUseCase throws

--- src/presentation/controllers/users/get-user-by-id/get-user-by-id-controller.ts
+++ src/presentation/controllers/users/get-user-by-id/get-user-by-id-controller.ts
@@ -1,12 +1,16 @@
 import { GetUserById } from '@/domain/usecases/users/get-user-by-id'
-import { noContent } from '@/presentation/helpers/http-helper'
+import { noContent, serverError } from '@/presentation/helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
 
 export class GetUserByIdController implements Controller {
   constructor(private readonly getUserByIdUseCase: GetUserById) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { userId } = httpRequest.body
-    await this.getUserByIdUseCase.execute(userId!)
-    return noContent()
+    try {
+      const { userId } = httpRequest.body
+      await this.getUserByIdUseCase.execute(userId!)
+      return noContent()
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
