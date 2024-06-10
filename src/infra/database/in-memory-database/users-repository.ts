c402fe48a6ddc36218be4601eb4c325ba342ff9f
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2024-06-10 12:35:31 -0300

    feat: ensure createUser returns null if email already exists

--- src/infra/database/in-memory-database/users-repository.ts
+++ src/infra/database/in-memory-database/users-repository.ts
@@ -2,8 +2,27 @@ import { CreateUserRepository } from '@/data/protocols/users/create-user-reposit
 import { GetUserByEmailRepository } from '@/data/protocols/users/get-user-by-email-repository'
 import { UserModel } from '@/domain/models/user-model'
 
-export class InMemoryUsersRepository implements GetUserByEmailRepository {
+export class InMemoryUsersRepository
+  implements CreateUserRepository, GetUserByEmailRepository
+{
   private repository: UserModel[] = []
+
+  async createUser(
+    params: CreateUserRepository.Params
+  ): Promise<CreateUserRepository.Result> {
+    const exists = await this.getByEmail(params.email)
+    if (!exists) {
+      const user = {
+        id: '1',
+        name: params.name,
+        email: params.email,
+        phone: params.phone
+      }
+      this.repository.push(user)
+      return user
+    }
+    return null
+  }
 
   async getByEmail(email: string): Promise<GetUserByEmailRepository.Result> {
     const found = this.repository.find((user) => user.email === email)
