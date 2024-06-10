deef93c91df83dd45f19c0230fd959c000f8ebd7
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2024-06-10 12:36:48 -0300

    feat: add domain use case to update user by id

---
+++ src/domain/usecases/users/update-user-by-id.ts
@@ -0,0 +1,14 @@
+import { UserModel } from '@/domain/models/user-model'
+import { Either } from '@/shared'
+
+export interface UpdateUserById {
+  execute(
+    userId: string,
+    params: UpdateUserById.Params
+  ): Promise<Either<Error, UpdateUserById.Result>>
+}
+
+export namespace UpdateUserById {
+  export type Params = Omit<UserModel, 'id'>
+  export type Result = UserModel
+}
