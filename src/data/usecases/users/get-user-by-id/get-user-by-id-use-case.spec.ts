580e936cf37f9ee680e9c399674d8a438333313f
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2024-06-10 12:36:14 -0300

    refactor: move sut creation to a factory method

--- src/data/usecases/users/get-user-by-id/get-user-by-id-use-case.spec.ts
+++ src/data/usecases/users/get-user-by-id/get-user-by-id-use-case.spec.ts
@@ -2,15 +2,32 @@ import { describe, it, expect, vi } from 'vitest'
 import { GetUserByIdRepository } from '../../../protocols/users/get-user-by-id-repository'
 import { GetUserByIdUseCase } from './get-user-by-id-use-case'
 
+const makeGetUserByIdRepositoryStub = (): GetUserByIdRepository => {
+  class GetUserByIdRepositoryStub implements GetUserByIdRepository {
+    async getById(_userId: string): Promise<GetUserByIdRepository.Result> {
+      return null
+    }
+  }
+  return new GetUserByIdRepositoryStub()
+}
+
+type SutTypes = {
+  sut: GetUserByIdUseCase
+  getUserByIdRepositoryStub: GetUserByIdRepository
+}
+
+const makeSut = (): SutTypes => {
+  const getUserByIdRepositoryStub = makeGetUserByIdRepositoryStub()
+  const sut = new GetUserByIdUseCase(getUserByIdRepositoryStub)
+  return {
+    sut,
+    getUserByIdRepositoryStub
+  }
+}
+
 describe('GetUserByIdUseCase', () => {
   it('Should call GetUserByIdRepository with correct param', async () => {
-    class GetUserByIdRepositoryStub implements GetUserByIdRepository {
-      async getById(_userId: string): Promise<GetUserByIdRepository.Result> {
-        return null
-      }
-    }
-    const getUserByIdRepositoryStub = new GetUserByIdRepositoryStub()
-    const sut = new GetUserByIdUseCase(getUserByIdRepositoryStub)
+    const { sut, getUserByIdRepositoryStub } = makeSut()
     const getUserSpy = vi.spyOn(getUserByIdRepositoryStub, 'getById')
     await sut.execute('any_id')
     expect(getUserSpy).toHaveBeenCalledOnce()
