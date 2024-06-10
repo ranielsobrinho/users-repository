81636999e91492fe6b2236e37386b6002fe49bd5
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2024-06-10 12:35:37 -0300

    refactor: move sut creation to a factory method

--- src/infra/database/postgres/users/users-postgres-repository.spec.ts
+++ src/infra/database/postgres/users/users-postgres-repository.spec.ts
@@ -1,60 +1,50 @@
 import { describe, expect, it } from 'vitest'
 import { InMemoryUsersRepository } from '../../in-memory-database/users-repository'
 
+const makeCreateUserRequest = () => ({
+  name: 'any_name',
+  email: 'any_email',
+  phone: 'any_phone'
+})
+
+const makeCreatedUser = () => ({
+  id: '1',
+  name: 'any_name',
+  email: 'any_email',
+  phone: 'any_phone'
+})
+
+const makeSut = () => {
+  return new InMemoryUsersRepository()
+}
 describe('UsersRepository', () => {
   describe('getByEmail', () => {
     it('Should return null if email is not taken', async () => {
-      const sut = new InMemoryUsersRepository()
+      const sut = makeSut()
       const user = await sut.getByEmail('any_email')
       expect(user).toBeNull()
     })
 
     it('Should return user if email is taken', async () => {
-      const sut = new InMemoryUsersRepository()
-      await sut.createUser({
-        name: 'any_name',
-        email: 'any_email',
-        phone: 'any_phone'
-      })
+      const sut = makeSut()
+      await sut.createUser(makeCreateUserRequest())
       const user = await sut.getByEmail('any_email')
-      expect(user).toEqual({
-        id: '1',
-        name: 'any_name',
-        email: 'any_email',
-        phone: 'any_phone'
-      })
+      expect(user).toEqual(makeCreatedUser())
     })
   })
 
   describe('createUser', () => {
     it('Should return null if email already exists', async () => {
-      const sut = new InMemoryUsersRepository()
-      await sut.createUser({
-        name: 'any_name',
-        email: 'any_email',
-        phone: 'any_phone'
-      })
-      const userCreated = await sut.createUser({
-        name: 'any_name',
-        email: 'any_email',
-        phone: 'any_phone'
-      })
+      const sut = makeSut()
+      await sut.createUser(makeCreateUserRequest())
+      const userCreated = await sut.createUser(makeCreateUserRequest())
       expect(userCreated).toBeNull()
     })
 
     it('Should return user created if email does not exists', async () => {
-      const sut = new InMemoryUsersRepository()
-      const userCreated = await sut.createUser({
-        name: 'any_name',
-        email: 'any_email',
-        phone: 'any_phone'
-      })
-      expect(userCreated).toEqual({
-        id: '1',
-        name: 'any_name',
-        email: 'any_email',
-        phone: 'any_phone'
-      })
+      const sut = makeSut()
+      const userCreated = await sut.createUser(makeCreateUserRequest())
+      expect(userCreated).toEqual(makeCreatedUser())
     })
   })
 })
