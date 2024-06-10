e0fb1ee99a7e920727263b47a9c6f2d543bef6f9
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2024-06-10 12:36:05 -0300

    feat: ensure ListAllUsersController returns 500 if ListAllUsers use case throws

--- src/presentation/controllers/users/list-all-users/list-all-users-controller.ts
+++ src/presentation/controllers/users/list-all-users/list-all-users-controller.ts
@@ -1,11 +1,15 @@
 import { ListAllUsers } from '@/domain/usecases/users/list-all-users'
-import { noContent } from '@/presentation/helpers/http-helper'
+import { noContent, serverError } from '@/presentation/helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
 
 export class ListAllUsersController implements Controller {
   constructor(private readonly listAllUsersUseCase: ListAllUsers) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.listAllUsersUseCase.execute()
-    return noContent()
+    try {
+      await this.listAllUsersUseCase.execute()
+      return noContent()
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
