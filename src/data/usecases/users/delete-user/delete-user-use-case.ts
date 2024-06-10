9ec0c0c8c78ca2720c5e5b8755dbb41632d7a4d2
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2024-06-10 12:36:35 -0300

    feat: ensure DeleteUserUseCase returns not found error if GetUserByIdRepository returns null

--- src/data/usecases/users/delete-user/delete-user-use-case.ts
+++ src/data/usecases/users/delete-user/delete-user-use-case.ts
@@ -1,11 +1,17 @@
 import { GetUserByIdRepository } from '@/data/protocols/users/get-user-by-id-repository'
 import { DeleteUserById } from '@/domain/usecases/users/delete-user-by-id'
-import { Either, right } from '@/shared'
+import { Either, right, left } from '@/shared'
+import { NotFoundError } from '@/data/errors/not-found-error'
 
 export class DeleteUserUseCase implements DeleteUserById {
   constructor(private readonly getUserByIdRepository: GetUserByIdRepository) {}
   async execute(userId: string): Promise<Either<Error, DeleteUserById.Result>> {
-    await this.getUserByIdRepository.getById(userId)
+    const user = await this.getUserByIdRepository.getById(userId)
+
+    if (!user) {
+      return left(new NotFoundError())
+    }
+
     return right({
       id: 'any_id',
       name: 'any_name',
