8cddcef4638f2f4373f946b1b985c72d4b766666
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2024-06-10 12:36:22 -0300

    refactor: move sut creation to a factory method

--- src/presentation/controllers/users/get-user-by-id/get-user-by-id-controller.spec.ts
+++ src/presentation/controllers/users/get-user-by-id/get-user-by-id-controller.spec.ts
@@ -2,28 +2,49 @@ import { describe, it, expect, vi } from 'vitest'
 import { GetUserById } from '../../../../domain/usecases/users/get-user-by-id'
 import { Either, right } from '../../../../shared'
 import { GetUserByIdController } from './get-user-by-id-controller'
+import { UserModel } from '../../../../domain/models/user-model'
+
+const makeUserModel = (): UserModel => ({
+  id: 'any_id',
+  name: 'any_name',
+  email: 'any_email',
+  phone: 'any_phone'
+})
+
+const makeGetUserRequest = () => ({
+  body: {
+    userId: 'any_id'
+  }
+})
+
+const makeGetUserByIdUseCaseStub = (): GetUserById => {
+  class GetUserByIdUseCaseStub implements GetUserById {
+    async execute(_userId: string): Promise<Either<Error, any>> {
+      return right(makeUserModel())
+    }
+  }
+  return new GetUserByIdUseCaseStub()
+}
+
+type SutTypes = {
+  sut: GetUserByIdController
+  getUserByIdUseCaseStub: GetUserById
+}
+
+const makeSut = (): SutTypes => {
+  const getUserByIdUseCaseStub = makeGetUserByIdUseCaseStub()
+  const sut = new GetUserByIdController(getUserByIdUseCaseStub)
+  return {
+    sut,
+    getUserByIdUseCaseStub
+  }
+}
 
 describe('GetUserByIdController', () => {
   it('Should call GetUserByIdUseCase with correct param', async () => {
-    class GetUserByIdUseCaseStub implements GetUserById {
-      async execute(_userId: string): Promise<Either<Error, any>> {
-        return right({
-          id: 'any_id',
-          name: 'any_name',
-          email: 'any_email',
-          phone: 'any_phone'
-        })
-      }
-    }
-    const getUserByIdUseCaseStub = new GetUserByIdUseCaseStub()
-    const sut = new GetUserByIdController(getUserByIdUseCaseStub)
+    const { sut, getUserByIdUseCaseStub } = makeSut()
     const getByIdSpy = vi.spyOn(getUserByIdUseCaseStub, 'execute')
-    await sut.handle({
-      body: {
-        userId: 'any_id'
-      }
-    })
-
+    await sut.handle(makeGetUserRequest())
     expect(getByIdSpy).toHaveBeenCalledOnce()
     expect(getByIdSpy).toHaveBeenCalledWith('any_id')
   })
