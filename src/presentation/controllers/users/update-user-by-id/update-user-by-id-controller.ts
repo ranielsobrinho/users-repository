de14d32ec44b0ba276d00534a16d993968c64eec
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2024-08-21 10:42:04 -0300

    feat: ensure UpdateUserController returns 500 if UpdateUserUseCase throws

--- src/presentation/controllers/users/update-user-by-id/update-user-by-id-controller.ts
+++ src/presentation/controllers/users/update-user-by-id/update-user-by-id-controller.ts
@@ -1,17 +1,22 @@
 import { UpdateUserById } from '@/domain/usecases/users/update-user-by-id'
-import { noContent } from '@/presentation/helpers/http-helper'
+import { noContent, serverError } from '@/presentation/helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
 
 export class UpdateUserController implements Controller {
   constructor(private readonly updateUserByIdUseCase: UpdateUserById) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { userId } = httpRequest.params
-    const { name, email, phone } = httpRequest.body
-    await this.updateUserByIdUseCase.execute(userId, {
-      name,
-      email,
-      phone
-    })
-    return noContent()
+    try {
+      const { userId } = httpRequest.params
+      const { name, email, phone } = httpRequest.body
+      await this.updateUserByIdUseCase.execute(userId, {
+        name,
+        email,
+        phone
+      })
+
+      return noContent()
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
