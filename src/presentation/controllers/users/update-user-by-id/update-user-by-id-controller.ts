fcf8fa203019d420c08184d0868ae29ed06f289c
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2024-08-21 10:43:29 -0300

    feat: ensure UpdateUserController returns 404 if UpdateUserUseCase returns not found error

--- src/presentation/controllers/users/update-user-by-id/update-user-by-id-controller.ts
+++ src/presentation/controllers/users/update-user-by-id/update-user-by-id-controller.ts
@@ -1,5 +1,9 @@
 import { UpdateUserById } from '@/domain/usecases/users/update-user-by-id'
-import { noContent, serverError } from '@/presentation/helpers/http-helper'
+import {
+  noContent,
+  notFound,
+  serverError
+} from '@/presentation/helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
 
 export class UpdateUserController implements Controller {
@@ -8,12 +12,15 @@ export class UpdateUserController implements Controller {
     try {
       const { userId } = httpRequest.params
       const { name, email, phone } = httpRequest.body
-      await this.updateUserByIdUseCase.execute(userId, {
+      const userOrError = await this.updateUserByIdUseCase.execute(userId, {
         name,
         email,
         phone
       })
 
+      if (userOrError.isLeft()) {
+        return notFound(userOrError.value)
+      }
       return noContent()
     } catch (error) {
       return serverError(error)
