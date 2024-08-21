12644119d9375f9b24ad1144d15294d5cd2bcbb9
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2024-08-21 10:57:52 -0300

    feat: ensure UpdateUserUseCase returns required field error if it is undefined

--- src/data/usecases/users/update-user-by-id/update-user-by-id-use-case.ts
+++ src/data/usecases/users/update-user-by-id/update-user-by-id-use-case.ts
@@ -1,8 +1,9 @@
-import { NotFoundError } from '@/data/errors'
+import { NotFoundError, RequiredFieldError } from '@/data/errors'
 import { GetUserByIdRepository } from '@/data/protocols/users/get-user-by-id-repository'
 import { UpdateUserByIdRepository } from '@/data/protocols/users/update-user-by-id-repository'
 import { UpdateUserById } from '@/domain/usecases/users/update-user-by-id'
 import { Either, left, right } from '@/shared'
+import { validate } from '@/data/utils/validate-params'
 
 export class UpdateUserByIdUseCase implements UpdateUserById {
   constructor(
@@ -18,6 +19,11 @@ export class UpdateUserByIdUseCase implements UpdateUserById {
 
     if (!user) {
       return left(new NotFoundError())
+    }
+
+    const validationError = await validate(params)
+    if (validationError.length) {
+      return left(new RequiredFieldError(validationError))
     }
 
     const updatedData = await this.updateUserByIdRepository.update(
