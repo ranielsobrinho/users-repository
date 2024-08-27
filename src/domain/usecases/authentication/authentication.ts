2d1a3888f8f2089bdf61c1f075723efa3ce9fa1b
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2024-08-27 18:19:43 -0300

    feat: add usecase to authenticate user

---
+++ src/domain/usecases/authentication/authentication.ts
@@ -0,0 +1,17 @@
+import { Either } from '@/shared'
+
+export type AuthenticationModel = {
+  email: string
+  phone: string
+}
+
+export interface Authentication {
+  auth(
+    params: Authentication.Params
+  ): Promise<Either<Error, Authentication.Result>>
+}
+
+export namespace Authentication {
+  export type Params = AuthenticationModel
+  export type Result = string
+}
