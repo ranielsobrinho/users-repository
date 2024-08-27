3a49fb3f700b4f872ad4425b98c9306ca0958238
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2024-08-27 20:22:51 -0300

    test: ensure TokenGenerator returns access token on success

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -37,5 +37,11 @@ describe('JwtAdapter', () => {
       const promise = sut.generate('any_id')
       await expect(promise).rejects.toThrow()
     })
+
+    it('Should return access token on success', async () => {
+      const sut = makeSut()
+      const accessToken = await sut.generate('any_id')
+      expect(accessToken).toBe('any_token')
+    })
   })
 })
