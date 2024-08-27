d9da7fd5d94d190ed69e6be0a9e163c5b1b60026
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2024-08-27 20:17:24 -0300

    refactor: move sut creation to a factory method

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -14,10 +14,14 @@ vi.mock('jsonwebtoken', async () => {
   }
 })
 
+const makeSut = (): JwtAdapter => {
+  return new JwtAdapter('secret')
+}
+
 describe('JwtAdapter', () => {
   describe('generate', () => {
     it('should call sign with correct params', async () => {
-      const sut = new JwtAdapter('secret')
+      const sut = makeSut()
       const generateTokenSpy = vi.spyOn(jwt, 'sign')
       await sut.generate('any_id')
       expect(generateTokenSpy).toHaveBeenCalledWith('any_id', 'secret', {
