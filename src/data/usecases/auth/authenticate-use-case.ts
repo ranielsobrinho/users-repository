687755d058be4a4f5a5e3e37d129eba3b31e746a
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2024-08-27 18:53:24 -0300

    feat: ensure AuthenticationUseCase returns left error if GetUserByEmailRepository returns null

--- src/data/usecases/auth/authenticate-use-case.ts
+++ src/data/usecases/auth/authenticate-use-case.ts
@@ -1,6 +1,7 @@
+import { NotFoundError } from '@/data/errors'
 import { GetUserByEmailRepository } from '@/data/protocols/users/get-user-by-email-repository'
 import { Authentication } from '@/domain/usecases/authentication/authentication'
-import { Either, right } from '@/shared'
+import { Either, left, right } from '@/shared'
 
 export class AuthenticateUseCase implements Authentication {
   constructor(
@@ -9,7 +10,12 @@ export class AuthenticateUseCase implements Authentication {
   async execute(
     params: Authentication.Params
   ): Promise<Either<Error, Authentication.Result>> {
-    await this.getUserByEmailRepository.getByEmail(params.email)
+    const userData = await this.getUserByEmailRepository.getByEmail(
+      params.email
+    )
+    if (!userData) {
+      return left(new NotFoundError())
+    }
     return right('ok')
   }
 }
