ad5e51554e2ede2dbc5dc5024c0bb6063d111763
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2024-10-01 09:44:40 -0300

    refactor: add tokenGenerator dependency

--- src/main/factories/usecases/users/create-user/create-user-use-case.ts
+++ src/main/factories/usecases/users/create-user/create-user-use-case.ts
@@ -1,8 +1,11 @@
 import { CreateUserUseCase } from '@/data/usecases/users/create-user/add-user-use-case'
 import { CreateUser } from '@/domain/usecases/users/create-user'
+import { JwtAdapter } from '@/infra/criptography/jwt-adapter/jwt-adapter'
 import { UsersRepository } from '@/infra/database/postgres/users/users-postgres-repository'
 
 export const makeCreateUserUseCase = (): CreateUser => {
+  const salt = process.env.JWT_SALT || 'knasdLKJASÃç12'
   const usersRepository = new UsersRepository()
-  return new CreateUserUseCase(usersRepository, usersRepository)
+  const tokenGenerator = new JwtAdapter(salt)
+  return new CreateUserUseCase(usersRepository, usersRepository, tokenGenerator)
 }
