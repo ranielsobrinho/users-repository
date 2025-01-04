593be606f38011c737a410307ac8341de7f5e82b
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2025-01-04 00:17:24 -0300

    feat: add new endpoint on routes

--- src/main/routes/user-routes.ts
+++ src/main/routes/user-routes.ts
@@ -5,9 +5,11 @@ import { makeListAllUsersController } from '../factories/controllers/users/list-
 import { makeGetUserByIdController } from '../factories/controllers/users/get-user-by-id/get-user-by-id-controller'
 import { makeUpdateUserByIdController } from '../factories/controllers/users/update-user-by-id/update-user-by-id-controller'
 import { makeDeleteUserByIdController } from '../factories/controllers/users/delete-user-by-id/delete-user-by-id-controller'
+import { makeLoginController } from '../factories/controllers/users/auth-user/auth-user-controller'
 
 export default (router: Router): void => {
   router.post('/signup', adaptRoute(makeCreateUserController()))
+  router.post('/login', adaptRoute(makeLoginController()))
   router.get('/users', adaptRoute(makeListAllUsersController()))
   router.get('/users/:userId', adaptRoute(makeGetUserByIdController()))
   router.delete('/users/:userId', adaptRoute(makeDeleteUserByIdController()))
