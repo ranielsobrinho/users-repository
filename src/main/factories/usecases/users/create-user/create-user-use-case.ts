69f066ba2f63e5b9e5dc2d755ae3a75afe716264
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2025-01-04 11:05:52 -0300

    refactor: add bcrypt adapter to usecase factory

--- src/main/factories/usecases/users/create-user/create-user-use-case.ts
+++ src/main/factories/usecases/users/create-user/create-user-use-case.ts
@@ -1,5 +1,6 @@
 import { CreateUserUseCase } from '@/data/usecases/users/create-user/add-user-use-case'
 import { CreateUser } from '@/domain/usecases/users/create-user'
+import { BcryptAdapter } from '@/infra/criptography/bcrypt-adapter/bcrypt-adapter'
 import { JwtAdapter } from '@/infra/criptography/jwt-adapter/jwt-adapter'
 import { UsersRepository } from '@/infra/database/postgres/users/users-postgres-repository'
 
@@ -7,5 +8,11 @@ export const makeCreateUserUseCase = (): CreateUser => {
   const salt = process.env.JWT_SALT || 'knasdLKJASÃç12'
   const usersRepository = new UsersRepository()
   const tokenGenerator = new JwtAdapter(salt)
-  return new CreateUserUseCase(usersRepository, usersRepository, tokenGenerator)
+  const bcryptAdapter = new BcryptAdapter(12)
+  return new CreateUserUseCase(
+    usersRepository,
+    usersRepository,
+    tokenGenerator,
+    bcryptAdapter
+  )
 }
