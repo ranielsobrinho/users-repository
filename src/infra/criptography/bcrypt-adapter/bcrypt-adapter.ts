19d1b2e9aa61cd8de437dadbc022c85eeb5968f8
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2025-01-07 11:37:18 -0300

    feat: should return false if compare fails

--- src/infra/criptography/bcrypt-adapter/bcrypt-adapter.ts
+++ src/infra/criptography/bcrypt-adapter/bcrypt-adapter.ts
@@ -11,7 +11,7 @@ export class BcryptAdapter implements Encrypter, HashComparer {
   }
 
   async compare(value: string, hash: string): Promise<boolean> {
-    await bcrypt.compare(value, hash)
-    return true
+    const isValid = await bcrypt.compare(value, hash)
+    return isValid
   }
 }
