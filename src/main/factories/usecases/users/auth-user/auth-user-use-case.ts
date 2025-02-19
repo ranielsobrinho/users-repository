95d046a13e47a7a4507297a8656ed91fc46b25ef
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2025-02-19 10:26:21 -0300

    refactor: add new dependency to auth usecase

--- src/main/factories/usecases/users/auth-user/auth-user-use-case.ts
+++ src/main/factories/usecases/users/auth-user/auth-user-use-case.ts
@@ -1,5 +1,6 @@
 import { AuthenticateUseCase } from '@/data/usecases/auth/authenticate-use-case'
 import { Authentication } from '@/domain/usecases/authentication/authentication'
+import { BcryptAdapter } from '@/infra/criptography/bcrypt-adapter/bcrypt-adapter'
 import { JwtAdapter } from '@/infra/criptography/jwt-adapter/jwt-adapter'
 import { UsersRepository } from '@/infra/database/postgres/users/users-postgres-repository'
 
@@ -7,5 +8,6 @@ export const makeAuthenticateUseCase = (): Authentication => {
   const salt = process.env.JWT_SALT || 'knasdLKJASÃç12'
   const usersRepository = new UsersRepository()
   const tokenGenerator = new JwtAdapter(salt)
-  return new AuthenticateUseCase(usersRepository, tokenGenerator)
+  const bcryptAdapter = new BcryptAdapter(12)
+  return new AuthenticateUseCase(usersRepository, tokenGenerator, bcryptAdapter)
 }
