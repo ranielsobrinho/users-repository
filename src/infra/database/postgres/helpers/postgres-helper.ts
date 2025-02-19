77653908423fa8500fb82b5fcd7c350e127463b2
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2025-02-19 10:25:24 -0300

    refactor: new way to connect to postgres database

--- src/infra/database/postgres/helpers/postgres-helper.ts
+++ src/infra/database/postgres/helpers/postgres-helper.ts
@@ -1,9 +1,19 @@
 import { Pool } from 'pg'
 
-const DB_CONNECTION_STRING: string = process.env.DB_CONNECTION_STRING!
+const DB_HOST: string = process.env.DB_HOST!
+const DB_USER: string = process.env.DB_USER!
+const DB_PASSWORD: string = process.env.DB_PASSWORD!
+const DB_NAME: string = process.env.DB_NAME!
+const DB_PORT: number = Number(process.env.DB_PORT!)
 
 class PostgresHelper {
-  private client = new Pool({ connectionString: DB_CONNECTION_STRING })
+  private client = new Pool({
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    port: DB_PORT
+  })
 
   async connect(): Promise<void> {
     this.client
