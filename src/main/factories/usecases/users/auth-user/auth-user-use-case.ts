9697355eb72956621efde72109e39f5a172c04e6
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2025-05-21 23:45:44 -0300

    refactor: improve code based on sonarqube hints

--- src/main/factories/usecases/users/auth-user/auth-user-use-case.ts
+++ src/main/factories/usecases/users/auth-user/auth-user-use-case.ts
@@ -5,7 +5,7 @@ import { JwtAdapter } from '@/infra/criptography/jwt-adapter/jwt-adapter'
 import { UsersRepository } from '@/infra/database/postgres/users/users-postgres-repository'
 
 export const makeAuthenticateUseCase = (): Authentication => {
-  const salt = process.env.JWT_SALT || 'knasdLKJASÃç12'
+  const salt = process.env.JWT_SALT ?? 'knasdLKJASÃç12'
   const usersRepository = new UsersRepository()
   const tokenGenerator = new JwtAdapter(salt)
   const bcryptAdapter = new BcryptAdapter(12)
