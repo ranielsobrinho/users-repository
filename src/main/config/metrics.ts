a5ff8eba980622528d28dfda0ee36f600b832463
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2025-07-02 00:04:57 -0300

    refactor: should receive app instance instead of creating a new server

--- src/main/config/metrics.ts
+++ src/main/config/metrics.ts
@@ -1,19 +1,12 @@
-import express, { Request, Response } from 'express'
 import client from 'prom-client'
-import logger from './logger'
+import { Express, Request, Response } from 'express'
 
-const app = express()
-
-const KEY = '[Metrics]:'
-export function startMetricsServer(): void {
+export function startMetricsServer(app: Express): void {
   const collectDefaultMetrics = client.collectDefaultMetrics
 
   collectDefaultMetrics()
   app.get('/metrics', async (_req: Request, res: Response) => {
     res.set('Content-Type', client.register.contentType)
     return res.send(await client.register.metrics())
-  })
-  app.listen(9101, () => {
-    logger.info(`${KEY} Metrics server started at http://localhost:9101`)
   })
 }
