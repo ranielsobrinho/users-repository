a366f0b0664d5507a9644d7499e172399ef6ed83
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2025-10-03 18:11:58 -0300

    feat: should return left error if required fields is undefined

--- src/data/usecases/clients/create-client/create-client-use-case.ts
+++ src/data/usecases/clients/create-client/create-client-use-case.ts
@@ -1,8 +1,9 @@
-import { EmailAlreadyInUseError } from '@/data/errors'
+import { EmailAlreadyInUseError, RequiredFieldError } from '@/data/errors'
 import { GetClientByEmailRepository } from '@/data/protocols/db/clients/get-client-by-email-repository'
+import { validate } from '@/data/utils/validate-params'
 import { ClientModel } from '@/domain/models/client-model'
 import { CreateClient } from '@/domain/usecases/clients/create-client'
-import { Either, right } from '@/shared'
+import { Either, left, right } from '@/shared'
 
 export class CreateClientUseCase implements CreateClient {
   constructor(
@@ -13,6 +14,11 @@ export class CreateClientUseCase implements CreateClient {
     params: CreateClient.Params
   ): Promise<Either<EmailAlreadyInUseError, ClientModel>> {
     const { email } = params
+    const validationError = await validate(params)
+    if (validationError.length) {
+      return left(new RequiredFieldError(validationError))
+    }
+
     await this.getClientByEmailRepository.getByEmail(email)
     return right({
       email: 'test',
