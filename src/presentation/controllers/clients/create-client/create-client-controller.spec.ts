c4893e5a3f22ac53afda4710ca4ef62483d708e6
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2025-10-14 21:57:52 -0300

    test: ensure CreateClientController returns 400 if CreateClient returns email already in use error

--- src/presentation/controllers/clients/create-client/create-client-controller.spec.ts
+++ src/presentation/controllers/clients/create-client/create-client-controller.spec.ts
@@ -6,6 +6,13 @@ import { CreateClient } from '../../../../domain/usecases/clients/create-client'
 import { ClientModel } from '../../../../domain/models/client-model'
 import { CreateClientController } from './create-client-controller'
 
+class EmailAlreadyInUseError extends Error {
+  constructor(email: string) {
+    super(`Email ${email} already in use.`)
+    this.name = 'EmailAlreadyInUseError'
+  }
+}
+
 const makeCreateClientResult = (): ClientModel => ({
   id: 'any_id',
   name: 'any_name',
@@ -57,12 +64,25 @@ describe('CreateClientController', () => {
     })
   })
 
-  it('Should return 500 if CreateUser throws', async () => {
+  it('Should return 500 if CreateClient throws', async () => {
     const { sut, createClientUseCaseStub } = makeSut()
     vi.spyOn(createClientUseCaseStub, 'execute').mockRejectedValueOnce(
       new Error()
     )
     const httpResponse = await sut.handle(makeCreateClientRequest())
     expect(httpResponse).toEqual(serverError(new Error()))
+  })
+
+  it('Should return 400 if CreateClient returns a EmailAlreadyInUse error', async () => {
+    const { sut, createClientUseCaseStub } = makeSut()
+    vi.spyOn(createClientUseCaseStub, 'execute').mockResolvedValueOnce(
+      left(new EmailAlreadyInUseError(makeCreateClientRequest().body.email))
+    )
+    const httpResponse = await sut.handle(makeCreateClientRequest())
+    expect(httpResponse).toEqual(
+      badRequest(
+        new EmailAlreadyInUseError(makeCreateClientRequest().body.email)
+      )
+    )
   })
 })
