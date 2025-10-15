c5f474b8aa046f07dbbf7cc027b0dd41b49024a0
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2025-10-14 22:26:50 -0300

    feat: ensure ClientsRepository implements GetClientByEmailRepository

--- src/infra/database/postgres/clients/clients-postgres-repository.ts
+++ src/infra/database/postgres/clients/clients-postgres-repository.ts
@@ -1,8 +1,11 @@
 import { randomUUID } from 'node:crypto'
 import { CreateClientRepository } from '@/data/protocols/db/clients/create-client-repository'
 import DatabaseHelper from '@/infra/database/postgres/helpers/postgres-helper'
+import { GetClientByEmailRepository } from '@/data/protocols/db/clients/get-client-by-email-repository'
 
-export class ClientsRepository implements CreateClientRepository {
+export class ClientsRepository
+  implements CreateClientRepository, GetClientByEmailRepository
+{
   async createClient(
     params: CreateClientRepository.Params
   ): Promise<CreateClientRepository.Result> {
@@ -12,6 +15,15 @@ export class ClientsRepository implements CreateClientRepository {
       [randomUUID(), params.name, params.email, params.phone]
     )
 
+    return result.rows[0] ?? null
+  }
+
+  async getByEmail(email: string): Promise<GetClientByEmailRepository.Result> {
+    const client = await DatabaseHelper.getClient()
+    const result = await client.query(
+      'SELECT id, name, email, phone FROM clients WHERE email = $1',
+      [email]
+    )
     return result.rows[0] ?? null
   }
 }
